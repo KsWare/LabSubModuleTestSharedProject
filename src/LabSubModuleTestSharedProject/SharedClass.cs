84ac05b9eeda979b9144ee39fff4e11c7517f973
SchreinerK <ks@ksware.de>
2019-09-13 20:20:03 +0200

    add shared class

---
+++ src/LabSubModuleTestSharedProject/SharedClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabSubModuleTestSharedProject
+{
+	public class SharedClass
+	{
+		public int GetInt32()
+		{
+			return 1;
+		}
+	}
+}
